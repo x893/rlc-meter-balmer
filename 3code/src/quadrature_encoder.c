098515b3ff5bd18f80938554b83b83c70d4fb1ea
balmer <balmer@inbox.ru>
2014-07-07 20:35:06 +0300

    - ??????? ???????? ??? ???????????? 3 ?????? ??? ?????????? ?????? ????????

--- 3code/src/quadrature_encoder.c
+++ 3code/src/quadrature_encoder.c
@@ -5,6 +5,7 @@
 #include "quadrature_encoder.h"
 #include "lcd_interface.h"
 
+
 //-------------------------------------------#DEFINE------------------------------------------
 #define Codeur_A           GPIO_Pin_6
 #define Codeur_A_SOURCE    GPIO_PinSource6
@@ -15,22 +16,64 @@
 #define Codeur_AF          GPIO_AF_2
  
 #define Codeur_TIMER       TIM3
+#define Codeur_TIMER_RCC    RCC_APB1Periph_TIM3
 
 #define BUTTON_PIN 			GPIO_Pin_5
 #define BUTTON_GPIO			GPIOA
 
 static bool g_lastButtonState = false;
+
+#ifdef DEF_INTERFACE_BUTTONS
+static bool g_lastButtonStateA = false;
+static bool g_lastButtonStateB = false;
+static void QuadInitAsButton();
+#else
 static uint16_t g_lastEncValue = 0;
+static void QuadInitAsEncoder();
+#endif
+
 
 void QuadTimerButton();
 
 void QuadEncInit()
 {
+
+#ifdef DEF_INTERFACE_BUTTONS
+    QuadInitAsButton();
+#else 
+    QuadInitAsEncoder();
+#endif
+
     GPIO_InitTypeDef GPIO_InitStructure;
+    
+    GPIO_InitStructure.GPIO_Pin = BUTTON_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(BUTTON_GPIO, &GPIO_InitStructure);
 
+    QuadTimerButton();
+}
+
+#ifdef DEF_INTERFACE_BUTTONS
+void QuadInitAsButton()
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    // 2 Inputs for A and B Encoder Channels
+    GPIO_InitStructure.GPIO_Pin = Codeur_A|Codeur_B;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(Codeur_GPIO, &GPIO_InitStructure);
+}
+#else //!DEF_INTERFACE_BUTTONS
+static void QuadInitAsEncoder()
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
     RCC_AHBPeriphClockCmd(Codeur_RCC, ENABLE);
-
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+    RCC_APB1PeriphClockCmd(Codeur_TIMER_RCC, ENABLE);
 
  
     // 2 Inputs for A and B Encoder Channels
@@ -59,15 +102,8 @@ void QuadEncInit()
     TIM_EncoderInterfaceConfig(Codeur_TIMER, TIM_EncoderMode_TI1, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
      
     TIM_Cmd(Codeur_TIMER, ENABLE);
-
-    GPIO_InitStructure.GPIO_Pin = BUTTON_PIN;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-    GPIO_Init(BUTTON_GPIO, &GPIO_InitStructure);
-
-    QuadTimerButton();
 }
+#endif
 
 uint16_t QuadEncValue()
 {
@@ -110,10 +146,20 @@ void TIM7_IRQHandler(void)
     {
         bool buttonState = QuadEncButton();
         if(buttonState && !g_lastButtonState)
-        {
             OnButtonPressed();   
-        }
         g_lastButtonState = buttonState;
+
+#ifdef DEF_INTERFACE_BUTTONS
+        bool buttonStateA = (GPIO_ReadInputDataBit(Codeur_GPIO, Codeur_A)==0);
+        if(buttonStateA && !g_lastButtonStateA)
+            OnWeel(-1);   
+        g_lastButtonStateA = buttonStateA;
+
+        bool buttonStateB = (GPIO_ReadInputDataBit(Codeur_GPIO, Codeur_B)==0);
+        if(buttonStateB && !g_lastButtonStateB)
+            OnWeel(+1);   
+        g_lastButtonStateB = buttonStateB;
+#else
         uint16_t encValue = QuadEncValue();
         if(encValue!=g_lastEncValue)
         {
@@ -121,6 +167,8 @@ void TIM7_IRQHandler(void)
         }
 
         g_lastEncValue = encValue;
+#endif
+
         OnTimer();
 
         TIM_ClearITPendingBit(TIM7, TIM_IT_Update);
